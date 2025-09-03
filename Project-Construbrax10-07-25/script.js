d65646647d5969ea86642ac2263e475dbd26861a
Kelermann014 <fkelermann@gmail.com>
2025-09-03 16:35:24 -0300

    Update script.js

--- Project-Construbrax10-07-25/script.js
+++ Project-Construbrax10-07-25/script.js
@@ -57,7 +57,6 @@ document.addEventListener('DOMContentLoaded', () => {
         
         atualizarSelectClientes();
         atualizarSelectFiadores();
-        atualizarSelectVeiculos();
     };
 
     // --- CONEXÃO DE EVENTOS ---
@@ -65,7 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn-gerenciar-fiadores').addEventListener('click', abrirModalGerenciarFiadores);
     document.getElementById('btn-abrir-simulacao').addEventListener('click', () => abrirModal('modalSimulacao'));
     document.getElementById('btn-salvar-cliente').addEventListener('click', salvarCliente);
-    document.getElementById('btn-abrir-novo-fiador-simulacao').addEventListener('click', () => abrirModalParaNovoFiador(true));
     document.getElementById('btn-abrir-cadastro-veiculo').addEventListener('click', abrirModalCadastroVeiculo);
     document.getElementById('btn-salvar-fiador').addEventListener('click', salvarFiador);
     document.getElementById('btn-simular').addEventListener('click', simularEmprestimo);
