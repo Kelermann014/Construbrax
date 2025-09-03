1da0ca71d71842e3b75fc3256ffbe7d448c443d9
Kelermann014 <fkelermann@gmail.com>
2025-09-03 12:53:49 -0300

    Update script.js

--- Project-Construbrax10-07-25/script.js
+++ Project-Construbrax10-07-25/script.js
@@ -103,4 +103,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // --- INICIALIZAÇÃO DA
+    // --- INICIALIZAÇÃO DA PÁGINA ---
+    atualizarInterfaceCompleta();
+});
